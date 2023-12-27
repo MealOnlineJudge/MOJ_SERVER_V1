ad7c14f520ad2dcf23543c01df67f145d999391d
qlido <jason7530@naver.com>
2023-12-27 15:20:20 +0900

    initial commit

---
+++ src/test/kotlin/com/bssm/moj/MojApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bssm.moj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MojApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
